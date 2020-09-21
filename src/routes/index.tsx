5c8d7d07ff4e26214dcbad208cb18362b0e8869d
vargattj <vargattjh@gmail.com>
2020-09-21 11:59:09 -0300

    styled components

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repositories from '../pages/Repositories';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} exact />
+      <Route path="/repository" component={Repositories} />
+    </Switch>
+  );
+};
+
+export default Routes;
